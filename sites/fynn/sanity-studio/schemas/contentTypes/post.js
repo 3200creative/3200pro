11a76d37ab30d6a305f8ae2ce56148f9be89a79a
3200 Creative <info@3200creative.com>
2020-09-02 19:07:03 -0500

    c32 updates

--- sites/fynn/sanity-studio/schemas/contentTypes/post.js
+++ sites/fynn/sanity-studio/schemas/contentTypes/post.js
@@ -9,13 +9,11 @@ export default {
       name: 'title',
       type: 'string',
       title: 'Title',
-      description: 'Titles should be catchy, descriptive, and not too long'
     },
     {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
-      description: 'Some frontends will require a slug to be set to be able to show the post',
       options: {
         source: 'title',
         maxLength: 96
@@ -25,7 +23,6 @@ export default {
       name: 'publishedAt',
       type: 'datetime',
       title: 'Published at',
-      description: 'This can be used to schedule post for publishing'
     },
     {
       name: 'featuredImage',
@@ -36,8 +33,6 @@ export default {
       name: 'excerpt',
       type: 'excerptPortableText',
       title: 'Excerpt',
-      description:
-        'This ends up on summary pages, on Google, when people share your post in social media.'
     },
     {
       name: 'blockContent',
