ba5519ff72a2a6841794595a2fdfd276b9a19509
3200 Creative <info@3200creative.com>
2021-01-08 15:16:18 -0600

    fynn initial commit

---
+++ sites/fynn/gatsby-node.js
@@ -0,0 +1,193 @@
+const { isFuture } = require("date-fns");
+
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  actions.createTypes([
+    schema.buildObjectType({
+      name: "SanityPost",
+      interfaces: ["Node"],
+      fields: {
+        isPublished: {
+          type: "Boolean!",
+          resolve: (source) => new Date(source.publishedAt) <= new Date(),
+        },
+      },
+    }),
+  ]);
+};
+
+async function createLandingPages(pathPrefix = "/", graphql, actions, reporter) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityRoute(filter: { slug: { current: { ne: null } } }) {
+        edges {
+          node {
+            id
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+
+  const routeEdges = (result.data.allSanityRoute || {}).edges || [];
+  routeEdges.forEach((edge) => {
+    const { id, slug = {} } = edge.node;
+    const path = [pathPrefix, slug.current, "/"].join("");
+    reporter.info(`Creating landing page: ${path}`);
+    createPage({
+      path,
+      component: require.resolve("./src/templates/landingPage.js"),
+      context: { id },
+    });
+  });
+}
+
+async function createBlogPostPages(pathPrefix = "/blog", graphql, actions, reporter) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityPost(filter: { slug: { current: { ne: null } }, isPublished: { eq: true } }) {
+        edges {
+          node {
+            id
+            publishedAt
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+
+  const postEdges = (result.data.allSanityPost || {}).edges || [];
+  postEdges
+    .filter((edge) => !isFuture(edge.node.publishedAt))
+    .forEach((edge, index) => {
+      // create prev and next on each posts render (for Blog Post Pagination, BPP)
+      const previous = index === postEdges.length - 1 ? null : postEdges[index + 1].node
+      const next = index === 0 ? null : postEdges[index - 1].node
+      //
+      const { id, slug = {} } = edge.node;
+      const path = `/${slug.current}/`;
+      reporter.info(`Creating Blog Post: ${path}`);
+      createPage({
+        path,
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: { 
+          id,
+          previous,
+          next, },
+      });
+
+    });
+    const postsPerPage = 12
+    const numPages = Math.ceil(postEdges.length / postsPerPage)
+
+        Array.from({ length: numPages }).forEach((_, i) => {
+          reporter.info(`Creating Archive Page For Blog`);
+            createPage({
+                path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+                component: require.resolve("./src/templates/blogArchive.js"),
+                context: {
+                    limit: postsPerPage,
+                    skip: i * postsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                },
+            });
+        });
+
+}
+
+
+async function createProductPages(pathPrefix = "/shop/", graphql, actions, reporter) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityProduct(filter: { slug: { current: { ne: null } } }) {
+        edges {
+          node {
+            id
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+
+  const productEdges = (result.data.allSanityProduct || {}).edges || [];
+  productEdges
+    .filter((edge) => !isFuture(edge.node.publishedAt))
+    .forEach((edge) => {
+      const { id, slug = {} } = edge.node;
+      const path = `${pathPrefix}/${slug.current}/`;
+      reporter.info(`Creating Product Page: ${path}`);
+      createPage({
+        path,
+        component: require.resolve("./src/templates/product.js"),
+        context: {
+          id },
+      });
+    });
+}
+
+async function createPostCateogryArchives(disregardThis = "/shop/", graphql, actions, reporter) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityCategory(filter: { slug: { current: { ne: null } } }) {
+        edges {
+          node {
+            id
+            title
+            _id
+            description
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+  const categoryEdges = (result.data.allSanityCategory || {}).edges || [];
+  // Pagination
+  categoryEdges
+    .forEach((edge) => {
+      const { id, slug = {} } = edge.node;
+      const path = `/${slug.current}/`;
+      reporter.info(`Creating Category Archive: ${path}`);
+      createPage({
+        path,
+        component: require.resolve("./src/templates/categoryArchive.js"),
+        context: { id },
+      });
+    });
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  await createLandingPages("/", graphql, actions, reporter);
+  await createBlogPostPages("/", graphql, actions, reporter);
+  await createProductPages("/shop", graphql, actions, reporter);
+  await createPostCateogryArchives("/category", graphql, actions, reporter);
+};
---
+++ sites/fynn/src/templates/LandingPage.js
@@ -0,0 +1,104 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import React, { useState } from "react";
+import { graphql } from "gatsby";
+import { SEO } from "c32-gatsby-theme-core"
+import  Layout  from "c32-gatsby-theme-core/src/components/layoutV2"
+import GraphQLErrorList from 'c32-gatsby-theme-core/src/components/graphql-error-lists'
+// Modular Blocks
+import ModularTextBlock from '../components/block-content/ModularTextBlock'
+import ButtonBlock from "../components/block-content/ButtonBlock";
+import HeroBlock from "../components/block-content/HeroBlock";
+import RecentPostFeed from "../components/block-content/RecentPostFeed"
+import RecentProductFeed from "../components/block-content/RecentProductFeed"
+import Spacer from "../components/block-content/Spacer"
+
+export const query = graphql`
+  query PageTemplateQuery($id: String!) {
+    route: sanityRoute(id: { eq: $id }) {
+      slug {
+        current
+      }
+      useSiteTitle
+      landingPage {
+        ...LandingPageInfo
+      }
+    }
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      openGraph {
+        title
+        description
+        image {
+          ...SanityImage
+        }
+      }
+    }
+  }
+`;
+
+const LandingPage = (props) => {
+  const { data, errors } = props;
+
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    );
+  }
+
+  const site = (data || {}).site;
+
+  if (!site) {
+    throw new Error(
+      'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
+    );
+  }
+  console.log();
+  const page = data.landingPage || data.route.landingPage;
+
+  const content = (page._rawContent || [])
+  .filter((c) => !c.disabled)
+  .map((c, i) => {
+    let el = null;
+    switch (c._type) {
+      case "modularTextBlock":
+        el = <ModularTextBlock key={c._key} {...c} blocks={c.text}/>;
+        break;
+      case "buttonBlock":
+        el = <ButtonBlock key={c._key} {...c} />;
+        break;
+      case "heroBlock":
+        el = <HeroBlock key={c._key} {...c} />;
+        break;
+      case "recentPostFeed":
+        el = <RecentPostFeed key={c._key} {...c} />;
+        break;
+      case "recentProductFeed":
+        el = <RecentProductFeed key={c._key} {...c} />;
+        break;
+      case "spacer":
+        el = <Spacer key={c._key} {...c} />;
+        break;
+      default:
+        el = null;
+    }
+    return el;
+  });
+  
+  const menuItems = page.navMenu && (page.navMenu.items || []);
+  const pageTitle = data.route && !data.route.useSiteTitle && page.title;
+
+  return (
+    <Layout navMenuItems={menuItems} textWhite={true}>
+      <SEO 
+       title={page.seo.metaTitle && page.seo.metaTitle} 
+       description={page.seo.metaDesc && page.seo.metaDesc} 
+       />
+      <div sx={{pt:22}}>{content}</div>
+    </Layout>
+  );
+};
+
+export default LandingPage;
