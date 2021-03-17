1f8fc673dc759f7f17ad540ecd8731aec01dfa9a
3200pro <info@3200.pro>
2021-03-17 18:29:55 -0500

    gz frm 2

--- sites/01TailwindStarter/src/tailwindcss-components/components/footer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import FourColumnFooter from 'tailwindcss-components/src/components/tailwindUI/footers/fourColumnFooter'
-
-const SiteFooter = () => <FourColumnFooter />
-
-export default SiteFooter
--- themes/tailwindcss-components/src/components/script/script.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Gist from 'react-gist'
-
-const Script = ({ scriptURL, ...props }) => (
-  <div>
-    <Gist id={scriptURL} />
-  </div>
-)
-export default Script
