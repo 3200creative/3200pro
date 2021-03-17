816e85e7d7cdcc8bd425c75f19a74fd411d7afd2
3200pro <info@3200.pro>
2021-03-17 18:32:12 -0500

    cc frm 9

---
+++ sites/customcars/src/components/block-content/Figure.js
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import { buildImageObj } from '../../lib/helpers'
+import { imageUrlFor } from '../../lib/image-url'
+import { getFluidGatsbyImage } from "gatsby-source-sanity"
+const sanityConfig = {projectId: 'yit7sywj', dataset: 'production'}
+const Figure = (props, {node}) => {
+  const fluidProps = getFluidGatsbyImage(props.asset._ref,{ maxWidth: 800 }, sanityConfig)
+  return (
+    <figure  sx={{
+      width: '100%',
+      mx: 'auto',
+      variant: 'variants.globalFigure'
+    }}>
+      {props.asset && (
+        <img sx={{
+          variant: 'variants.shadow',
+          maxWidth: '100%',
+          width: props.fullWidth ? '100%' : null,
+          margin: '0 auto',
+          float: 'none',
+          display: 'block',
+        }}
+        src={imageUrlFor(buildImageObj(props))
+          .width(1200)
+          .url()}
+        alt={props.ccAlt}
+        />
+      )}
+      <figcaption>{props.caption}</figcaption>
+    </figure>
+  )
+}
+
+export default Figure
