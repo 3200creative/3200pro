1f92284cc9da02b6ff7548c6494ef61f7c3f2abd
3200pro <info@3200.pro>
2021-04-05 15:17:47 -0500

    32 updates 19

--- sites/3200pro/gatsby-config.js
+++ sites/3200pro/gatsby-config.js
@@ -45,14 +45,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-csp`,
       options: {
-        disableOnDev: false,
-        reportOnly: false,
-        mergeScriptHashes: false,
-        mergeStyleHashes: false,
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
         directives: {
-          'script-src': "'self' www.google-analytics.com typeform.com blob:",
-          'style-src': "'self' 'unsafe-inline' blob:",
-          'img-src': "'self' data: www.google-analytics.com blob:",
+          'script-src': "'self' www.google-analytics.com admin.typeform.com",
+          'style-src': "'self' 'unsafe-inline'",
+          'img-src': "'self' data: www.google-analytics.com",
+          // you can add your directives or override defaults
         },
       },
     },
