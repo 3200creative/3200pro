3962688321bb076a885220105e096a005ae0feda
3200pro <info@3200.pro>
2021-04-05 12:46:48 -0500

    32 updates 6

--- sites/3200pro/gatsby-config.js
+++ sites/3200pro/gatsby-config.js
@@ -43,24 +43,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: false, // you can disable scripts sha256 hashes
-        mergeStyleHashes: false, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
-
-        directives: {
-          'script-src': "'self' www.google-analytics.com",
-          'script-src': "'self' admin.typeform.com",
-          'style-src': "'self' 'unsafe-inline'",
-          'img-src': "'self' data: www.google-analytics.com",
-          // you can add your directives or override defaults
-        },
-      },
-    },
-    {
       resolve: `gatsby-plugin-disqus`,
       options: {
         shortname: `3200pro`,
